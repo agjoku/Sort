e109857e02d8c52c3139a03193c99bf62011a623
BuildTools <unconfigured@null.spigotmc.org>
2022-06-18 02:49:27 +0900

    first commit

---
+++ bubbleSortInput.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class bubbleSortInput {
+    public static void main(String[] args) {
+        int array[] = new  int[10];
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("数値を入力してください");
+        int num = scanner.nextInt();
+        System.out.println("入力した数値は" + num + "です。");
+        bubbleSortInput.array(array);
+    }
+
+    public static void array(int[] array) {
+        int numarray[] = new int[10];
+        int i;
+        for (i = 0; i < 10; i++) {
+            System.out.println("配列" + (i + 1) + "を入力");
+            Scanner scanner = new Scanner(System.in);
+            numarray[i] = scanner.nextInt();
+            System.out.println("配列" + i + "に" + numarray[i] + "を入力");
+        }
+        for (i = 0; i < 10; i++) {
+            System.out.print("|" + (i + 1) + ":");
+            System.out.print(numarray[i]);
+            //System.out.print("|");
+        }
+        System.out.print("|");
+    }
+}
