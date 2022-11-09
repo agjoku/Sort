30e4038c1e04725129dfdfe0f21144049e87eefe
agjoku <65605668+agjoku@users.noreply.github.com>
2022-11-09 14:18:33 +0900

    add comment out

--- bubbleSortInput.java
+++ bubbleSortInput.java
@@ -1,11 +1,13 @@
 import java.util.Scanner;
 public class bubbleSortInput {
     public static void main(String[] args) {
+        //入力した値を返すプログラム
         int array[] = new  int[10];
         Scanner scanner = new Scanner(System.in);
         System.out.println("数値を入力してください");
         int num = scanner.nextInt();
         System.out.println("入力した数値は" + num + "です。");
+        //10回任意の値を入力しバブルソートを行うプログラム
         bubbleSortInput.array(array);
     }
 
